fcd62f0a61ed9634aad8b6211ef04a91be592602
github username <ianjpollack@gmail.com>
2018-12-04 02:22:59 -0500

    test showing img / test showing details

--- cypress/integration/code_challenge_tests.js
+++ cypress/integration/code_challenge_tests.js
@@ -3,6 +3,7 @@ function alphabeticalShows(shows) {
 
 }
 
+// DOES THE SHOW OBJECT HAVE A POSTER IMAGE:
 const show = {name: "Wolcott, Jr.", posterImg: undefined};
 function checkShowPosterImg(show) {
   if(show.posterImg === undefined) {
@@ -15,7 +16,25 @@ function checkShowPosterImg(show) {
 describe("Check if show has poster img", function() {
   context("img", function() {
     it("has poster img", function() {
-      expect(checkShowPosterImg(show)).to.eq(true)
+      expect(checkShowPosterImg(show)).to.eq(true);
+    })
+  })
+})
+
+// DOES CLICKING ON SHOW DETAILS BRING UP THE MODAL:
+const state = {showShowDetails: true}
+function showingDetails(state) {
+  if(state.showShowDetails === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+describe("Check showing show's details modal", function() {
+  context("state status", function() {
+    it("it is showing show details", function() {
+      expect(showingDetails(state)).to.eq(true);
     })
   })
 })
